36c8feaef0396b641fc1506cd709f61e3a467958
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-02-26 09:30:10 -0800

    Initial Commit

---
+++ Login-3D/Login_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_3DApp.swift
+//  Login-3D
+//
+//  Created by Sothesom on 07/12/1402.
+//
+
+import SwiftUI
+
+@main
+struct Login_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
